84d5d893ffdc678de673379380a056b640e93752
一家人xwc <xmcr1125@qq.com>
2019-10-25 10:54:21 +0800

    init

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/chain5j/log15
+
+go 1.13
+
+require github.com/go-stack/stack v1.8.0
---
+++ tests/log_test.go
@@ -0,0 +1,52 @@
+// description: log 
+// 
+// @author: xwc1125
+// @date: 2019/9/18
+package tests
+
+import (
+	"github.com/chain5j/log15"
+	"os"
+	"testing"
+)
+
+var(
+	ostream  log.Handler
+	glogger  *log.GlogHandler
+)
+
+func TestLog(t *testing.T) {
+	// 颜色
+	//usecolor := (isatty.IsTerminal(os.Stderr.Fd()) || isatty.IsCygwinTerminal(os.Stderr.Fd())) && os.Getenv("TERM") != "dumb"
+	//output := io.Writer(os.Stderr)
+	//if usecolor {
+	//	output = colorable.NewColorableStderr()
+	//}
+	//ostream = log.StreamHandler(output, log.TerminalFormat(usecolor))// usecolor表示日志是否颜色显示
+	ostream = log.StreamHandler(os.Stderr, log.TerminalFormat(true))// usecolor表示日志是否颜色显示
+	glogger = log.NewGlogHandler(ostream)
+	glogger.Verbosity(log.Lvl(5))// 日志级别
+
+	//glogger.Vmodule(*vmodule)
+	log.Root().SetHandler(glogger)
+
+	// all loggers can have key/value context
+	srvlog := log.New("module", "app/server")
+
+	// all log messages can have key/value context
+	srvlog.Warn("abnormal conn rate", "rate", "curRate", "low", "lowRate", "high", "highRate")
+
+	// child loggers with inherited context
+	connlog := srvlog.New("raddr", "127.0.0.1")
+	connlog.Info("connection open")
+
+	// lazy evaluation
+	connlog.Debug("ping remote", "latency", log.Lazy{"pingRemote"})
+
+	// flexible configuration
+	srvlog.SetHandler(log.MultiHandler(
+		log.StreamHandler(os.Stderr, log.LogfmtFormat()),
+		log.LvlFilterHandler(
+			log.LvlError,
+			log.Must.FileHandler("errors.json", log.JSONFormat()))))
+}
